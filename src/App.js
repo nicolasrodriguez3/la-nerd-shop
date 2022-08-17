f612659fa13dbfa045ede87c93966e6fbcaea4ab
Nicolas Rodriguez <nicolasrodriguez@outlook.com.ar>
2022-08-17 12:24:56 -0300

    add carousel from materialize

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Carousel from "./components/Carousel"
 import Header from "./components/Header"
 import Hero from "./components/Hero"
 import SectionTitle from "./components/SectionTitle"
@@ -9,6 +10,7 @@ function App() {
 			<Header />
 			<Hero />
 			<SectionTitle>HOLA</SectionTitle>
+			<Carousel />
 		</div>
 	)
 }
---
+++ src/components/Carousel/index.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useRef } from "react";
+import M from "materialize-css";
+import one from "../../assets/img/hero-bg.jpg";
+import two from "../../assets/img/hero-bg.jpg"
+import three from "../../assets/img/hero-bg.jpg"
+import four from "../../assets/img/hero-bg.jpg"
+import five from "../../assets/img/hero-bg.jpg"
+
+function Carousel(){
+	const refCarousel = useRef(null)
+
+
+  useEffect(() => {
+    const options = {
+      duration: 200,
+			indicators: true,
+    };
+		
+    const instance = M.Carousel.init(refCarousel.current, options);
+		return () => {
+			instance.destroy();
+		}
+  }, [])
+
+    return (
+      <div
+        ref={refCarousel}
+        className="carousel"
+      >
+        <div className="carousel-item">
+          <img alt="1" src={one} />
+        </div>
+        <div className="carousel-item">
+          <img alt="2" src={two} />
+        </div>
+        <div className="carousel-item">
+          <img alt="3" src={three} />
+        </div>
+        <div className="carousel-item">
+          <img alt="4" src={four} />
+        </div>
+        <div className="carousel-item">
+          <img alt="5" src={five} />
+        </div>
+      </div>
+    )
+}
+
+export default Carousel;
